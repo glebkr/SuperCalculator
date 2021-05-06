dd993e1797dd20a0cf9ea99d970c06c22bd453ea
glebkr <77020700+glebkr@users.noreply.github.com>
2021-05-06 21:03:58 +0300

    Добавление файлов

---
+++ SuperCalculator/SuperCalculator/OperatorHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace SuperCalculator
+{
+    public static class OperatorHelper
+    {
+       public static double Calculate(double value1, double value2, string myOperator)
+        {
+            double result = 0;
+            switch(myOperator)
+            {
+                case "÷":
+                    result = value1 / value2;
+                    break;
+                case "*":
+                    result = value1 * value2;
+                    break;
+                case "+":
+                    result = value1 + value2;
+                    break;
+                case "-":
+                    result = value1 - value2;
+                    break;
+            }
+            return result;
+        }
+    }
+}
